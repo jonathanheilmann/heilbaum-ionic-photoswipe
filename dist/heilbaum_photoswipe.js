743ea7b3a10f9ee386db698d62511fbb368f0a60
jonathanheilmann <mail@jonathan-heilmann.de>
2016-11-21 20:05:45 +0100

    Changed compiler module

--- dist/heilbaum_photoswipe.js
+++ dist/heilbaum_photoswipe.js
@@ -1,4 +1,25 @@
-export { HeilBaumPhotoSwipeModule } from './src/module';
-export { HeilBaumPhotoSwipeComponent } from './src/heilbaum_photoswipe.component';
-export { HeilBaumPhotoSwipeController } from './src/heilbaum_photoswipe';
+System.register(['./src/module', './src/heilbaum_photoswipe.component', './src/heilbaum_photoswipe'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters:[
+            function (module_1_1) {
+                exports_1({
+                    "HeilBaumPhotoSwipeModule": module_1_1["HeilBaumPhotoSwipeModule"]
+                });
+            },
+            function (heilbaum_photoswipe_component_1_1) {
+                exports_1({
+                    "HeilBaumPhotoSwipeComponent": heilbaum_photoswipe_component_1_1["HeilBaumPhotoSwipeComponent"]
+                });
+            },
+            function (heilbaum_photoswipe_1_1) {
+                exports_1({
+                    "HeilBaumPhotoSwipeController": heilbaum_photoswipe_1_1["HeilBaumPhotoSwipeController"]
+                });
+            }],
+        execute: function() {
+        }
+    }
+});
 //# sourceMappingURL=heilbaum_photoswipe.js.map
